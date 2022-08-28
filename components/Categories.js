c029363e619f73ec35836eb2ce389e9c29cb527b
BlackEyedGhouL <34senith@gmail.com>
2022-08-28 18:32:42 +0530

    Read all categories from firestore

--- components/Categories.js
+++ components/Categories.js
@@ -1,8 +1,23 @@
 import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CategoryCard from "./CategoryCard";
+import { db } from "../Core/Config";
+import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
 
 const Categories = () => {
+  const [categories, setCategories] = useState();
+
+  useEffect(() => {
+    const q = query(collection(db, "categories"), orderBy("name"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const categories = [];
+      querySnapshot.forEach((doc) => {
+        categories.push(doc.data());
+      });
+      setCategories(categories);
+    });
+  }, []);
+
   return (
     <ScrollView
       contentContainerStyle={{
@@ -12,30 +27,13 @@ const Categories = () => {
       horizontal
       showsHorizontalScrollIndicator={false}
     >
-      <CategoryCard
-        imgUrl="https://links.papareact.com/gn7"
-        title="Testing 1"
-      />
-      <CategoryCard
-        imgUrl="https://links.papareact.com/gn7"
-        title="Testing 2"
-      />
-      <CategoryCard
-        imgUrl="https://links.papareact.com/gn7"
-        title="Testing 3"
-      />
-      <CategoryCard
-        imgUrl="https://links.papareact.com/gn7"
-        title="Testing 3"
-      />
-      <CategoryCard
-        imgUrl="https://links.papareact.com/gn7"
-        title="Testing 3"
-      />
-      <CategoryCard
-        imgUrl="https://links.papareact.com/gn7"
-        title="Testing 3"
-      />
+      {categories.map((category) => (
+        <CategoryCard
+          key={category.id}
+          imgUrl={category.image}
+          title={category.name}
+        />
+      ))}
     </ScrollView>
   );
 };
